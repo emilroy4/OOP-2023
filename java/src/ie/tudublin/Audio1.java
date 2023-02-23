ccc031cf05eb0da873559963ca43e821bf9aba92
emilroy4 <92442806+emilroy4@users.noreply.github.com>
2023-02-23 16:05:46 +0000

    Added case 2

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -6,8 +6,7 @@ import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import processing.core.PApplet;
 
-public class Audio1 extends PApplet
-{
+public class Audio1 extends PApplet {
     Minim minim;
     AudioPlayer ap;
     AudioInput ai;
@@ -20,10 +19,10 @@ public class Audio1 extends PApplet
     float smoothedAmplitude = 0;
 
     public void keyPressed() {
-		if (key >= '0' && key <= '9') {
-			mode = key - '0';
-		}
-		if (keyCode == ' ') {
+        if (key >= '0' && key <= '9') {
+            mode = key - '0';
+        }
+        if (keyCode == ' ') {
             if (ap.isPlaying()) {
                 ap.pause();
             } else {
@@ -31,87 +30,71 @@ public class Audio1 extends PApplet
                 ap.play();
             }
         }
-	}
-
-    public void settings()
-    {
-        size(1024, 1000, P3D);
-        //fullScreen(P3D, SPAN);
     }
 
-    public void setup()
-    {
+    public void settings() {
+        size(1024, 1000);
+        // Use the default JAVA2D renderer
+    }
+
+    public void setup() {
         minim = new Minim(this);
         // Uncomment this to use the microphone
-         //ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
-         //ab = ai.mix; 
+        // ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
+        // ab = ai.mix;
 
         // And comment the next two lines out
         ap = minim.loadFile("drugs.mp3", 1024);
+        if (ap == null) {
+            println("Failed to load audio file!");
+            exit();
+        }
         ap.play();
         ab = ap.mix;
         colorMode(HSB);
 
         y = height / 2;
         smoothedY = y;
-
     }
 
-    float off = 0;
-
-    public void draw()
-    {
-        //background(0);
-        float halfH = height / 2;
-        float average = 0;
-        float sum = 0;
-        off += 1;
+    public void draw() {
         // Calculate sum and average of the samples
-        // Also lerp each element of buffer;
-        for(int i = 0 ; i < ab.size() ; i ++)
-        {
-            sum += abs(ab.get(i));
+        float sum = 0;
+        for (int i = 0; i < ab.size(); i++) {
+            float sample = ab.get(i);
+            sum += sample * sample; // square each sample to get the amplitude
         }
-        average= sum / (float) ab.size();
+        float average = sqrt(sum / (float) ab.size()); // take the square root to get the RMS amplitude
 
         smoothedAmplitude = lerp(smoothedAmplitude, average, 0.1f);
-        
-        float cx = width / 2;
-        float cy = height / 2;
 
         switch (mode) {
-			case 0:
+            case 0:
                 background(0);
-                for(int i = 0 ; i < ab.size() ; i ++)
-                {
-                    //float c = map(ab.get(i), -1, 1, 0, 255);
+                for (int i = 0; i < ab.size(); i++) {
                     float c = map(i, 0, ab.size(), 0, 255);
                     stroke(c, 255, 255);
-                    float f = ab.get(i) * halfH;
-                    line(i, halfH + f, i, halfH - f);                    
+                    float f = ab.get(i) * height / 2;
+                    line(i, height / 2 + f, i, height / 2 - f);
                 }
                 break;
-        case 1:
-            background(0);            
-            break;
-
+            case 1:
+                background(0);
+                float lerpedA = smoothedAmplitude * 100;
+                stroke(255);
+                fill(100, 255, 255);
+                circle(width / 2, height / 2, lerpedA);
+                circle(100, y, 50);
+                y += random(-10, 10);
+                smoothedY = lerp(smoothedY, y, 0.1f);
+                circle(200, smoothedY, 50);
+                break;
+            case 2:
+                //
+                break;
+            default:
+                println("Invalid mode: " + mode);
+                break;
         }
-        
-
-
-        
-        // Other examples we made in the class
-        /*
-        stroke(255);
-        fill(100, 255, 255);        
-        
-        circle(width / 2, halfH, lerpedA * 100);
-
-        circle(100, y, 50);
-        y += random(-10, 10);
-        smoothedY = lerp(smoothedY, y, 0.1f);        
-        circle(200, smoothedY, 50);
-        */
-
-    }        
+    }
 }
