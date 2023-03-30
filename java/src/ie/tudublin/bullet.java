c0331e6770129bc33bad18ed07fc37c55d80e2b8
emilroy4 <emilroy100@gmail.com>
2023-03-30 09:01:15 +0100

    .

--- java/src/ie/tudublin/bullet.java
+++ java/src/ie/tudublin/bullet.java
@@ -5,23 +5,49 @@ import processing.core.PVector;
 
 public class bullet {
     PVector pos;
+    float rot;
     float speed;
     PVector forward;
     PApplet p;
-}
+    int c;
 
-public bullet(float x, float y, PApplet p) {
-    pos = new PVector(x, y);
-    forward = new PVector(0, -1);
-    speed = 5;
-    this.p = p;
-    this.rot = rot;
-}
+    public bullet(float x, float y, float rot, int c, PApplet p)
+    {
+        pos = new PVector(x, y);
+        forward = new PVector(0, -1);
+        speed = 5;
+        this.p = p;
+        this.rot = rot;
+        this.c = c;
+        
 
-void render()   {
-    p.pushMatrix();
-    p.translate(pos.x,pos.y);
-    p.rotate(rot);
-    p.line(0,-5,-,5);
-    p.popMatrix();
+    }
+
+    void render()
+    {
+        p.pushMatrix();
+        p.stroke(c, 255, 255);
+        p.translate(pos.x, pos.y);
+        p.rotate(rot);
+        p.line(0, -5, 0, 5);
+        p.popMatrix();
+    }
+
+    void move()
+    {
+        forward.x = PApplet.sin(rot);
+        forward.y = - PApplet.cos(rot);
+
+        pos.add(PVector.mult(forward, speed));
+
+        if (pos.x < 0 || pos.x > p.width || pos.y < 0 || pos.y > p.height)
+        {
+            ((YASC)p).bullets.remove(this);
+        }
+
+
+    }
+
+
+
 }
