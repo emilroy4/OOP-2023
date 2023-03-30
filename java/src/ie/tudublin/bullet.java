cc848f8d1edb9b9ffc297d1faa88c4ed8ea5858f
emilroy4 <emilroy100@gmail.com>
2023-03-30 09:10:26 +0100

    .

--- java/src/ie/tudublin/bullet.java
+++ java/src/ie/tudublin/bullet.java
@@ -3,7 +3,7 @@ package ie.tudublin;
 import processing.core.PApplet;
 import processing.core.PVector;
 
-public class bullet {
+public class Bullet {
     PVector pos;
     float rot;
     float speed;
@@ -11,7 +11,7 @@ public class bullet {
     PApplet p;
     int c;
 
-    public bullet(float x, float y, float rot, int c, PApplet p)
+    public Bullet(float x, float y, float rot, int c, PApplet p)
     {
         pos = new PVector(x, y);
         forward = new PVector(0, -1);
